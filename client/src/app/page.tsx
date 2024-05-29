0ba5018d04234878113ee7a88ffa1fc3e09feee5
Nurul Islam <nishojib@gmail.com>
2024-05-29 18:47:40 -0400

    refactor(client): removing api call to check if it works

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,19 +1,20 @@
-import axios from 'axios';
-import { Suspense } from 'react';
+// import axios from 'axios';
+// import { Suspense } from 'react';
 
 export default async function Home() {
-  const data = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/health`, {
-    headers: {
-      'Content-Type': `application/json`,
-    },
-    withCredentials: true,
-  });
+  // const data = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/health`, {
+  //   headers: {
+  //     'Content-Type': `application/json`,
+  //   },
+  //   withCredentials: true,
+  // });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Suspense fallback={<p>Loading...</p>}>
+      {/* <Suspense fallback={<p>Loading...</p>}>
         {JSON.stringify(data.data, null, 2)}
-      </Suspense>
+      </Suspense> */}
+      Hello World
     </main>
   );
 }
