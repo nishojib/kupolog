d7f380e720d06ebc12df50ca2d32e79383e64358
Nurul Islam <nishojib@gmail.com>
2024-05-31 01:56:19 -0400

    refactor(client): renamed client to frontend

--- frontend/src/app/lib/axios.ts
+++ frontend/src/app/lib/axios.ts
@@ -4,9 +4,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
