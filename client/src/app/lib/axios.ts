7d888708626e5f34791efb18a37e35fcdc2da581
Nurul Islam <nishojib@gmail.com>
2024-05-30 22:37:52 -0400

    bugfix(client): testing to see if adding url directly works

--- client/src/app/lib/axios.ts
+++ client/src/app/lib/axios.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://api.kupolog.com',
   headers: {
     'Content-Type': 'application/json',
   },
